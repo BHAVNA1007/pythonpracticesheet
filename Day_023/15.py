2aace9ea49d41bf829115b161a9d1de3e94d6c3a
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-05-14 23:31:10 +0530

    added 15 pattern file

---
+++ Day_023/15.py
@@ -0,0 +1,26 @@
+'''
+A
+BB
+CCC
+DDDD
+EEEEE
+
+'''
+n = int(input("Enter a number: "))
+
+i = 1
+ch = 65
+while i <= n:
+   print()
+
+   j = 1
+  
+
+   while j <= i:
+       print(chr(ch), end="")
+       
+
+       j += 1
+   ch += 1 
+   i += 1
+       
