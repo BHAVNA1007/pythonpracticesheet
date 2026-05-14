aee846023e11c13f49472b4c72909f30ba7b51b7
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-05-14 23:41:34 +0530

    added 17 pattern file

---
+++ Day_023/17.py
@@ -0,0 +1,27 @@
+'''
+*
+##
+***
+####
+*****
+'''
+
+n = int(input("Enter a number: "))
+
+i = 1
+
+while i <= n:
+   print()
+
+   j = 1
+
+   while j <= i:
+
+      if i % 2 == 0:
+         print("#", end="")
+      else:
+         print("*", end="")
+
+      j += 1
+
+   i += 1 
