949b157b5c72ba1093c80be0ecab26c353519e41
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-05-14 20:30:53 +0530

    added Day_023 pattern 01 done

---
+++ Day_023/01_raw_patterns.py
@@ -0,0 +1,20 @@
+'''
+1)raw patterns
+
+********
+'''	
+
+n = int(input('Enter a number: '))
+
+i = 1
+
+while i<=1 :
+
+    print()
+    j = 1
+    while j<=i:
+       print("*"*n,end=' ')
+       j+=1
+    
+    i+=1
+
