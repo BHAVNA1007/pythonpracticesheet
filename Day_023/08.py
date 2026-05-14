f94e49a1ef1f6597642b1afb70fb24cbc10f581d
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-05-14 22:44:46 +0530

    added 08 pattern file

---
+++ Day_023/08.py
@@ -0,0 +1,21 @@
+'''
+*
+**
+***
+****
+*****
+'''
+
+n = int(input("Enter a number: "))
+
+i = 1
+
+while i<=n:
+    print()
+
+    j = 1
+    while j<=i:
+       print("*", end="")
+
+       j+=1
+    i += 1
