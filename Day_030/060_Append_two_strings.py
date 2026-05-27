807721fc8e46d9e22b13a325475144e7ec5f2fee
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-05-27 22:00:15 +0530

    updated  file 60

--- Day_030/060_Append_two_strings.py
+++ Day_030/060_Append_two_strings.py
@@ -2,3 +2,36 @@
 060_Append_two_strings but remove adjacent duplicates. S1="miss", S2="issippi" "misisip
 
 '''
+s1 = input('Enter s1: ')
+s2 = input('Enter s2: ')
+
+prev1 = s1[0]
+res1 = prev1
+
+i = 1
+
+while i < len(s1):
+   if s1[i] != prev1:
+      res1 += s1[i]
+      prev1 = s1[i]
+    
+   i+=1
+
+
+       
+prev2 = s2[0]
+res2 = prev2
+
+i = 1
+
+while i < len(s2):
+   if s2[i] != prev2:
+      res2 += s2[i]
+      prev2 = s2[i]
+    
+   i+=1
+
+res = res1 + res2
+print(res)
+
+
