91e51a801730e4380ff18ea0a1f56a434d20b1c2
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-06-01 19:13:24 +0530

    updated 072.py

--- Day_030/072.py
+++ Day_030/072.py
@@ -4,14 +4,11 @@ S = "abc", n = 2 "ab, bc
 
 '''
 s = input('Enter s string: ')
-n = int(input('Enter a length: '))
+n = int(input('Enter a length for substrings: '))
 
 for i in range(len(s)):
     for j in range(i,len(s)):
-        sub = s[i:j+1]          
- 
-          
-          if match == 1:
-       print(s[i]) 
-
-             
+        sub = s[i:j+1]
+        
+        if n== len(sub):  
+           print(sub,end=" ")           
