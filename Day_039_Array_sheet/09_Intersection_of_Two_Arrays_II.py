4983d4ef179b9e607b8d785df1fbf1f36722be11
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-06-13 11:18:43 +0530

    update

--- Day_039_Array_sheet/09_Intersection_of_Two_Arrays_II.py
+++ Day_039_Array_sheet/09_Intersection_of_Two_Arrays_II.py
@@ -12,3 +12,24 @@ Output: [4,9]
 Explanation: [9,4] is also accepted.
  
 '''
+
+n = int(input("Enter size of arr1: "))
+nums1 = []
+for i in range(n):
+    nums1.append(int(input()))
+print(nums1)    
+
+n2 = int(input("Enter size of arr2: "))
+nums2 = []
+for i in range(n2):
+    nums2.append(int(input()))
+print(nums2)
+
+res = []
+for i in nums1:
+    if i in nums2:
+        res.append(i)
+print(res)            
+            
+            
+    
