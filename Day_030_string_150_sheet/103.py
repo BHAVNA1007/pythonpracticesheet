afb64024e02aa2672e9833edaf18611586f8ef2f
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-07-01 22:09:30 +0530

    103 updated

--- Day_030_string_150_sheet/103.py
+++ Day_030_string_150_sheet/103.py
@@ -4,15 +4,19 @@ S = "((()))" TRUE
 
 '''
 
-s=input("Enter s: ")
-open_p=0
-close_p=0
+s = input("Enter paranthisis: ")
+
+open_paran = 0
+close_paran = 0
+
 for i in s:
-    if i=="(":
-        open_p+=1
-    elif  i==")":
-        close_p+=1
-if open_p==close_p:
-    print("True")
+   if i == "(":
+       open_paran += 1
+   elif i == ")":
+       close_paran += 1  
+
+if open_paran == close_paran:
+   print("TRUE")
+
 else:
-    print("False")
+   print("FALSE")
