b0c3a767c9eb4376bbed8e9c9e011eabaae44b20
BHAVNA1007 <bhavnapotphode7@gmail.com>
2026-07-23 23:27:49 +0530

    inheritance updated

--- Day_050_oops_assign/04_Multilevel_Inheritance.py
+++ Day_050_oops_assign/04_Multilevel_Inheritance.py
@@ -45,3 +45,74 @@ Loan Amount : ₹500000
 Interest Rate : 8.5%
 Loan Tenure : 5 Years
 '''
+
+class Person:
+
+   def __init__(self, name, age, mob):
+
+        self.name = name
+        self.age = age
+        self.mob = mob
+
+   def display(self):
+   
+       print("Name: ", self.name)
+       #print("Age: ", self.age)
+       #print("Mobile No: ", self.mob)
+
+class Customer(Person):
+
+   def __init__(self, name, age, mob, cust_id, acc_no):
+
+       super().__init__(name, age, mob)
+
+       self.cust_id = cust_id
+       self.acc_no = acc_no
+
+   def cust_info(self):
+  
+      print("Customer Id: ", self.cust_id)
+      print("Account  No: ", self.acc_no)
+ 
+class LoanAccount(Customer):
+  
+   def __init__(self, name, age, mob, cust_id, acc_no, loan_amount, rate, loan_tenure):
+
+      super().__init__(name, age, mob, cust_id, acc_no)
+
+      self.loan_amount = loan_amount
+      self.rate = rate
+      self.loan_tenure = loan_tenure
+
+
+   def loan_info(self):
+
+      print("Loan Amount: ", self.loan_amount)
+      print("Interest Rate: ", self.rate)
+      print("Loan Tenure: ", self.loan_tenure)
+   
+      
+cust_name = input("Customer Name: ")
+age = int(input("Age: "))
+mob = int(input("Mobile: "))
+
+
+cust_id = input("Customer Id: ")
+acc_no = int(input("Account Number: "))
+
+
+loan_amount = int(input("Loan Amount: "))
+rate = float(input("Interest Rate: "))
+loan_tenure = int(input("Loan Tenure: "))
+
+l = LoanAccount(cust_name, age, mob, cust_id, acc_no, loan_amount, rate, loan_tenure)
+
+print("\n----------- Loan Details -----------\n")
+
+l.display()
+l.cust_info()
+print()
+l.loan_info()
+
+
+
--- Day_050_oops_assign/05_Hierarchical_Inheritance.py
+++ Day_050_oops_assign/05_Hierarchical_Inheritance.py
@@ -52,3 +52,159 @@ Course : Python Full Stack
 Marks : 89
 
 '''
+
+class Person:
+
+   def __init__(self, name, age, address):
+  
+       self.name = name
+       self.age = age
+       self.address = address
+
+   def display(self):
+
+       print("Name: ", self.name)
+       print("Age: ", self.age)
+       print("Address: ", self.address)
+
+class Student(Person):
+
+    def __init__(self, name, age, address, roll_no, course, marks):
+        super().__init__(name, age, address)
+
+        self.roll_no = roll_no
+        self.course = course
+        self.marks = marks
+
+    def student_info(self):
+       
+        print("Roll No: ", self.roll_no)
+        print("Course: ", self.course)
+        print("Marks: ", self.marks) 
+        
+  
+class Teacher(Person):
+
+    def __init__(self, name, age, address, emp_id, sub, salary):
+        super().__init__(name, age, address)
+ 
+        self.emp_id = emp_id
+        self.sub = sub
+        self.salary = salary
+
+    def teacher_info(self):
+  
+        print("Employee Id: ", self.emp_id)
+        print("Subject: ", self.sub)
+        print("Salary: ", self.salary)
+
+class Principal(Person):
+
+   def __init__(self, name, age, address, emp_id, salary, office_num, exp, qualification):
+
+       super().__init__(name, age, address)
+        
+       self.emp_id = emp_id
+       self.salary = salary
+       self.office_num = office_num
+       self.exp = exp
+       self.qualification = qualification
+
+   def principal_info(self):
+
+       print("Employee ID:", self.emp_id)
+       print("Salary:", self.salary)
+ 
+       print("Office Number: ", self.office_num)
+       print("Experience: ", self.exp)
+       print("Qualification: ", self.qualification) 
+
+student = None
+teacher = None
+principal = None
+
+while True:
+
+   print("\n========== School ERP ==========\n")
+   print("1. Add Student")
+   print("2. Add Teacher")
+   print("3. Add Principal")
+   print("4. Display Student")
+   print("5. Display Teacher")
+   print("6. Display Principal")
+   print("7. Exit")
+
+   choice = int(input("\nEnter choice: "))
+
+   match choice:
+      
+      case 1:
+            s_name = input("Enter Name: ")
+            s_age = int(input("Enter Age: "))
+            s_add = input("Enter Address: ")
+            s_roll = int(input("Enter Roll Number: "))
+            s_course = input("Enter Course Name: ")
+            s_marks = int(input("Enter Marks: "))
+
+            student = Student(s_name, s_age, s_add, s_roll, s_course, s_marks)
+
+            print("Student added successfully....")
+
+
+      case 2:
+            t_name = input("Enter Name: ")
+            t_age = int(input("Enter Age: "))
+            t_add = input("Enter Address: ")
+            t_id = int(input("Enter Employee ID: "))
+            t_sub = input("Enter Subject Name: ")
+            t_salary = int(input("Enter Salary: "))
+
+            teacher = Teacher(t_name, t_age, t_add,  t_id, t_sub, t_salary)
+
+            print("Teacher added successfully....")
+
+
+      case 3:
+            p_name = input("Enter Name: ")
+            p_age = int(input("Enter Age: "))
+            p_add = input("Enter Address: ")
+            p_emp = int(input("Enter employee id"))
+            p_salary = int(input("Enter salary: "))
+            p_of_no = int(input("Enter Office Number: "))
+            p_exp = input("Enter Experience: ")
+            p_quali = input("Enter Qualification: ")
+
+            principal = Principal(p_name, p_age, p_add, p_emp, p_salary, p_of_no, p_exp, p_quali)
+
+            print("Principal added successfully....")
+
+      case 4:
+            if student:
+               student.display()
+               student.student_info()
+
+            else:
+               print("Student not available add firstly") 
+ 
+      case 5:
+            if teacher:
+               teacher.display()
+               teacher.teacher_info()
+
+            else:
+               print("Teacher not available add firstly") 
+
+      case 6:
+            if principal:
+               principal.display()
+               principal.principal_info()
+  
+
+            else:
+               print("Principal not available add firstly") 
+
+      case 7:
+              print("Exit............")
+              break 
+
+ 
